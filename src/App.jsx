6200073567d6d1456f39edf4f6650e3993a766e4
Bryan Gallo <bryangallo95@hotmail.com>
2023-10-17 07:28:40 -0500

    Inicio Proyecto Bebidas

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Container } from "react-bootstrap";
+function App() {
+    return (
+        <>
+            <header className="py-5">
+                <h1>BEBIDAS BRYAN</h1>
+            </header>
+            <Container>
+                <h1>Inicio Proyecto</h1>
+            </Container>
+        </>
+    );
+}
+
+export default App;
