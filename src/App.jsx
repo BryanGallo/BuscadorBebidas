e9247e9b97fd6827e07a35c4c0b00df657241988
Bryan Gallo <bryangallo95@hotmail.com>
2023-10-17 23:39:02 -0500

    Categorias Provider - Categorias del API

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { Container } from "react-bootstrap";
 import Formulario from "./components/Formulario";
+import { CategoriasProvider } from "./context/CategoriasProvider";
 function App() {
     return (
-        <>
+        <CategoriasProvider>
             <header className="py-5">
                 <h1>BEBIDAS BRYAN</h1>
             </header>
-            <Container>
+            <Container className="mt-5">
                 <Formulario />
             </Container>
-        </>
+        </CategoriasProvider>
     );
 }
 
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,9 @@
 import { Button, Form, Row, Col } from "react-bootstrap";
+import useCategorias from "../hooks/useCategorias";
 
 const Formulario = () => {
+    const { categorias } = useCategorias();
+
     return (
         <Form>
             <Row>
@@ -16,19 +19,29 @@ const Formulario = () => {
                     </Form.Group>
                 </Col>
                 <Col md={6}>
-                    <Form.Group>
-                        <Form.Label htmlFor="categoria">Categoria Bebida</Form.Label>
+                    <Form.Group className="mb-3">
+                        <Form.Label htmlFor="categoria">
+                            Categoria Bebida
+                        </Form.Label>
                         <Form.Select id="categoria" name="categoria">
                             <option>Elegir Categoria</option>
-                            <option>Cerveza</option>
-                            <option>Agua</option>
-                            <option>Jugo</option>
-                            <option>Cerveza</option>
-                            <option>Agua</option>
-                            <option>Jugo</option>
-                            <option>Cerveza</option>
+                            {categorias.map((categoria) => (
+                                <option
+                                    key={categoria.strCategory}
+                                    value={categoria.strCategory}
+                                >
+                                    {categoria.strCategory}
+                                </option>
+                            ))}
                         </Form.Select>
                     </Form.Group>
+                </Col>
+            </Row>
+            <Row className="justify-content-end">
+                <Col md={3}>
+                    <Button className="text-text-uppercase w-100" variant="danger">
+                        Buscar Bebida
+                    </Button>
                 </Col>
             </Row>
         </Form>
